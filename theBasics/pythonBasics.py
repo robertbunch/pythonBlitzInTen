19a63f4e16cc63e2188687c4728b70ecb35798e3
Robert Bunch <rdbunch@gmail.com>
2023-08-04 17:36:10 -0400

    Beginning of 8 - Update PythonBasics

---
+++ theBasics/pythonBasics.py
@@ -0,0 +1,29 @@
+# Comments = are anything that is NOT for python
+# Comments are for the devoloper/human
+
+# variable assignment is var name on left, an =, value on right
+name = "Rob" # assigning name to Rob
+# Snake case is used in Python when you have a variable 
+# name that has more than one word. The words are 
+# separated by _
+# camel case (alternative to snake) means starting each
+# new word with a capital letter, except the first
+#example: cookieCount
+cookie_count = 12 # assigning cookie_count to 12
+print("=========expecting 12=============")
+print(cookie_count)
+cookie_count = cookie_count - 2 #2 cookies were taken out
+print("=========expecting 10=============")
+print(cookie_count)
+cookie_count = cookie_count + 10 #bake 10 new cookies
+print("=========expecting 20=============")
+print(cookie_count)
+
+name = "Rob"
+name = 'Rob'
+# name = `Rob` Python will not like this!
+
+print("""They and was They and was They and was 
+      They and was They and was They and was 
+      They and was They and was They and was 
+      They and was They and was They and was """)
