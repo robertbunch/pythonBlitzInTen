075f986313857ca455cbbfdc07ed22f87f417cfe
Robert Bunch <rdbunch@gmail.com>
2023-08-04 15:41:01 -0400

    Start of 8 - data types

---
+++ pythonBasics.py
@@ -0,0 +1,19 @@
+# Comments - is anything Python should NOT run
+# Comments are here for the developer/Human
+
+#we made the variable "name" = the word "Rob"
+name = "Rob" # this is a variable assignment!
+name2 = 'Alex' #this works just fine!
+# name3 = `Akash` -- back ticks don't work anymore!
+# print(name)
+# print(name2)
+# snake case = means setting each word appart with an _
+# camel case = means setting each new word with a capital letter, except the first
+cookie_count = 12
+print(cookie_count)
+# cookie_count = 10
+# print(cookie_count)
+cookie_count = cookie_count - 2
+cookie_count = cookie_count + 10
+print("============expect next line to be 20==============")
+print(cookie_count) #expect this to be 20
