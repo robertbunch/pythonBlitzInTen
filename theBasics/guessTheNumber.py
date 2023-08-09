2103ea8fd095abc566485c6cb4ed4b48454832a1
Robert Bunch <rdbunch@gmail.com>
2023-08-09 14:07:45 -0400

    Ready for 16 - updated challenge

---
+++ theBasics/guessTheNumber.py
@@ -0,0 +1,13 @@
+
+#1. Assign a variable to an integer between 1 and 10
+correct_number = 3
+#2. Ask the user to guess a number between 1 and 10
+#3. Cast the number the user gave you into an int
+#3b. Cast the correct number into a string
+#4. If the user's number equals the correct number,
+#5. if so, print an awesome message to the screen
+#6. if not, print a sad message to the screen
+#7. add to the message what the number was
+    #sorry that is not correct. the number was 3
+#8. if the number is within 1, print a message that they were close
+    #and what the number was
