78dc966aa1c46b81772d5fb4f04b58071d9bc75b
Robert Bunch <rdbunch@gmail.com>
2023-08-08 22:00:33 -0400

    Beginning of 11 - if/else - spacing

---
+++ theBasics/conditionalsAndSpacing.py
@@ -0,0 +1,18 @@
+
+name = input("Please enter your name > ")
+# print(name)
+
+# what if we want to say something differnet based
+# on who the person is?
+if(name == "Rob"): #an if statement takes () and in the () we put Left thing == Right thing
+	print("Hello, instructor " + name)
+	#python is an indented language!
+else:
+	print("Hello, Student")
+	print("Hello, Student")
+	print("Hello, Student")
+	print("Hello, Student")
+	print("Uh uh")
+	print("Hello, Student")
+print("Out of conditionals statement")
+
