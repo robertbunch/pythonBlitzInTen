7390cd193214e40d4521b92a4616c4f20751551b
Robert Bunch <rdbunch@gmail.com>
2023-08-09 14:16:44 -0400

    Ready for 17 - guess a number solution

--- theBasics/guessTheNumber.py
+++ theBasics/guessTheNumber.py
@@ -2,12 +2,24 @@
 #1. Assign a variable to an integer between 1 and 10
 correct_number = 3
 #2. Ask the user to guess a number between 1 and 10
+guessed_number = input("Please guess a number between 1 and 10 > ")
 #3. Cast the number the user gave you into an int
+guessed_number_as_int = int(guessed_number)
 #3b. Cast the correct number into a string
+correct_number_as_string = str(correct_number)
 #4. If the user's number equals the correct number,
-#5. if so, print an awesome message to the screen
-#6. if not, print a sad message to the screen
-#7. add to the message what the number was
+if(guessed_number_as_int == correct_number):
+    #5. if so, print an awesome message to the screen
+    print("You are awesome! You guessed it!")
+elif(guessed_number_as_int + 1 == correct_number):
+    #8. if the number is within 1, print a message that they were close
+        #and what the number was    
+    print("You were close! The number was " + str(correct_number))
+elif(guessed_number_as_int - 1 == correct_number):
+    print("You were close! The number was " + str(correct_number))
+else:
+    #6. if not, print a sad message to the screen
+    # print("Sorry, that is not the number")
+    #7. add to the message what the number was
     #sorry that is not correct. the number was 3
-#8. if the number is within 1, print a message that they were close
-    #and what the number was
+    print("Sorry, that is not the number. The number was " + str(correct_number))
