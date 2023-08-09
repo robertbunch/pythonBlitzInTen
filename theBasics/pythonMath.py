fd736caf808e3fead78e2e5c7385832b05671aff
Robert Bunch <rdbunch@gmail.com>
2023-08-09 13:31:38 -0400

    Beginning of 14 - added python math

---
+++ theBasics/pythonMath.py
@@ -0,0 +1,39 @@
+
+# # operator - smybol/sign that can manipulate/or do 
+#     # something with an operand
+# # operand - is something that an operator affects
+
+# a_number = 3 + 5
+# ########Python operators
+# # + = addition/plus sign when used with numbers (float / int)
+# a_number = 5 + 3.14
+# # + = concatenate symbol when used with strings
+# a_string = "Rob " + "Bunch"
+# # nonsense = "Rob " + 3 #this will break!
+# # nonsense = 3 + "Rob " #this will break!
+
+# # - = subtraction/minus sign when used with 2 numbers
+# # - = a negative sign when used before a number
+# negative_number = -2
+# a_number = 5 - 3.14
+
+# # * = multiplication
+# a_product = 10 * 2 #20
+# # / = division 
+# a_quotient = 10 / 2 #5
+# # > = greater than symbol
+# # < = less than symbol
+# # print(3 < 2)
+# # >= ---> greater than or equal to
+# # <= ---> less than or equal to
+# print(3 < 3)
+# print(3 <= 3)
+# # = ---> assigning something
+# # == ---> equal to/comparing 2 things for a match
+# # != ---> NOT equal to/comparing 2 things for not a match
+# print(2 != 3) #True
+# print(2 != 2) #False
+
+print("2" == 2)
+print(2 == 2) #True
+
