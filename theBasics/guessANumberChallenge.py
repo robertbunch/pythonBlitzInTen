97b22a72228db8fcbb51b634a53c39c58853d2d5
Robert Bunch <rdbunch@gmail.com>
2023-08-09 13:38:09 -0400

    guess a number challenge 1

---
+++ theBasics/guessANumberChallenge.py
@@ -0,0 +1,13 @@
+
+# 1. Assign an integer between 1 and 10 to a variable
+correct_number = 3
+# 2. Ask the user for a number between 1 and 10
+
+# 3. Cast the user input to a number
+    # or cast the number you created into an integer
+# 4. Compare the two
+# 4a. Congrat user if they are the same
+# 5. If, the number is within 2, print "You were close, 
+    # the number was X"
+# 6. If the number is not within 2, print "You weren't close the number was X"
+# 7. If the number is correct, congrat with some awesome message
