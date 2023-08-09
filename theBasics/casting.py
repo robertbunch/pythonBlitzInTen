87c5686e7a2a9c60a7834718d3fe6f3ec34fc137
Robert Bunch <rdbunch@gmail.com>
2023-08-09 13:31:53 -0400

    Ready for 15 - added casting

---
+++ theBasics/casting.py
@@ -0,0 +1,11 @@
+
+# casting = change a variables data type without changing the variable itself
+a_number_as_string = "2" #string
+print(int(a_number_as_string) + 2)
+print(a_number_as_string + str(2))
+print(float(3))
+
+fav_number = input("What is your favorate number? > ")
+print(type(fav_number))
+fav_number_plus_three = int(fav_number) + 3
+
