0c506919a222e76ce1da9f5ef715ed23215c6ee4
Robert Bunch <rdbunch@gmail.com>
2023-08-09 14:25:24 -0400

    Ready for 18 - f-string formating

---
+++ theBasics/interpolationStrings.py
@@ -0,0 +1,8 @@
+
+# interpolation = co-mingling variables and strings together!
+cookie_count = 12
+print(f"The cookie count is cookie_count {cookie_count}")
+
+country = "England"
+person = f"Person's name is Joe. Joe is {45 - 13} and Joe is from {country}"
+print(person)
