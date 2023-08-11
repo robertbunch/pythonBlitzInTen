ec6216b30755476c860215141d3c038f5ac2c458
Robert Bunch <rdbunch@gmail.com>
2023-08-11 14:09:32 -0400

    Ready for 21

---
+++ theBasics/loops.py
@@ -0,0 +1,25 @@
+
+# a loop is a way to run the same code over and over
+# and stop when x happens, or when we have done it x times
+
+#while loop goes until x is not true
+counter = 0
+while(counter < 10):
+    # print(counter)
+    counter = counter + 1
+print("Loop has ended!")
+
+#for loop
+# i = the dish you are on
+# range(0,10) = the 10 dirty dishes in the sink
+for i in range(0,10):
+    if(i == 5):
+        print("Five")
+    else:    
+        print(i)
+
+
+
+
+
+
