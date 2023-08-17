59e97168d18b7650fe2301df4a6be55cf414330a
Robert Bunch <rdbunch@gmail.com>
2023-08-17 12:52:12 -0400

    Ready for 35 - added key/name args

--- theBasics/function.py
+++ theBasics/function.py
@@ -6,6 +6,10 @@
 # can send it variables
 # method is a funciton that belongs to a variable (object)
 
+#when we define a function (def) the () are known as
+#the siganture. The varaibles inside the siganture
+#are known as parameters
+#by default, they come in order
 def calc(operation,x,y):
     print(f"You have called Calc. You have sent the {operation} operation.")
     if(operation == "Sum"):
@@ -16,7 +20,13 @@ def calc(operation,x,y):
         #multiply x * y
         print(x * y)
 
+#when we call/invoke a function, the varaibles in ()
+#are called arguements
+#by default the args pass into the params in order
+#UNLESS, we use the key/name of the param
 print("Line 1")
-calc("Sum",2,9)
+calc("Sum",2,9) #call/invoke the function calc... which we defined above
 print("Line 2")
-calc("Mult",10,20)
+calc("Mult",10,20) #call/invoke the function calc... which we defined above
+calc(x = 9, y = 3, operation="Mult")
+# calc(9, 3, "Mult")
