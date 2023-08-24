f9202bd5cac41dd7c73e6799f8f6b8870f298d4b
Robert Bunch <rdbunch@gmail.com>
2023-08-24 19:46:03 -0400

    Init game.py

---
+++ text_rpg/game.py
@@ -0,0 +1,14 @@
+
+#the entry point (this is the file we run python on)
+#for our text based RPG
+
+hero_name = input("What is thy name, brave adventurer? > ")
+print(f"What would you like to do, {hero_name}?")
+print(f"1. Fight a monster")
+print(f"2. Go to the shop")
+print(f"3. Do a dance")
+action = input(" > ")
+
+
+
+
