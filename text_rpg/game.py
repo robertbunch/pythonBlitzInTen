e8adf2905a8d076e13334b85114b37ca8c4e7627
Robert Bunch <rdbunch@gmail.com>
2023-08-25 15:02:36 -0400

    Ready for 71 - add fight function  - fight loop

--- text_rpg/game.py
+++ text_rpg/game.py
@@ -36,7 +36,7 @@ enemies.append(zombie)
 
 main_options = [
     {
-        "text" : "Fight a monster",
+        "text" : "Go into the dark forest in search of adventure and loot",
         "input_key" : "1",
     },
     {
@@ -53,6 +53,16 @@ main_options = [
     }
 ]
 
+#if the player decides to fight, this function will run
+def fight():
+    enemy_to_fight = enemies[0]
+    in_fight = True #boolean that keeps us in the fight loop
+    while(in_fight):
+        print(f"{the_hero['name']}, thou hast encountered the fearsome {enemy_to_fight['name']}. The battle has begun!")
+        battle_action = input("What would you like to do? > ") #this is the chioce the player will make in the fight
+        print("Fight has ended")
+        in_fight = False
+
 #make a boolean that will control our main game loop
 game_on = True
 while(game_on):
@@ -60,7 +70,9 @@ while(game_on):
     for option in main_options:
         print(f"{option['input_key']}. {option['text']}")
     action = input(" > ") #this will BLOCK the loop, until the user answer
-    if(action == "q"):
+    if(action == "1"):
+        fight()
+    elif(action == "q"):
         game_on = False
 
 
