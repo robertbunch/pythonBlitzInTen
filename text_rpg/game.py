b712861e3e489137087ed1f5f2de6b4baf67c5e8
Robert Bunch <rdbunch@gmail.com>
2023-08-25 18:27:53 -0400

    Ready for 74 - Update battle mechanics

--- text_rpg/game.py
+++ text_rpg/game.py
@@ -19,11 +19,27 @@ def fight():
     random_index = random.randint(0,total_enemies-1)
     enemy_to_fight = game_data.enemies[random_index]
     in_fight = True #boolean that keeps us in the fight loop
+    print(f"{game_data.the_hero['name']}, thou hast encountered the {enemy_to_fight['adjective']} {enemy_to_fight['name']}. The battle has begun!")
     while(in_fight):
-        print(f"{game_data.the_hero['name']}, thou hast encountered the {enemy_to_fight['adjective']} {enemy_to_fight['name']}. The battle has begun!")
+        print(f"Thou hast {game_data.the_hero['hp']} health points. Thine enemy has {enemy_to_fight['hp']} health points.")
+        #this loop is the back and forth during a battle. Until someone wins
+        print(f"1. Attack with thine {game_data.the_hero['weapon']}.")
+        health_potion_count = game_data.the_hero['inventory'].count("health_potion")
+        print(f"2. Drink the health potion. Thou hast {health_potion_count} potions.")
+        print(f"3.Defend and heal.")
+        print(f"4. Do a little dance.")
+        print(f"5. Flee for your miserable, pitiful life.")
         battle_action = input("What would you like to do? > ") #this is the chioce the player will make in the fight
-        print("Fight has ended")
-        in_fight = False
+    #hero's turn
+        if(battle_action == "1"):
+            #user has chosen to attack
+            #hero_power_hit = take the hero's attack_power - monster's defense
+            #reduce the monster's hp by hero_power_hit
+            hero_power_hit = game_data.the_hero['attack_power'] - enemy_to_fight['defense']
+            enemy_to_fight['hp'] -= hero_power_hit
+        elif(battle_action == "5"):
+            print("Fight has ended")
+            in_fight = False
 
 #make a boolean that will control our main game loop
 game_on = True
