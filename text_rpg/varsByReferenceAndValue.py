0ff244c240e719e90fff387114b208b6c2010b07
Robert Bunch <rdbunch@gmail.com>
2023-08-28 11:59:30 -0400

    Ready for 78

--- text_rpg/varsByReferenceAndValue.py
+++ text_rpg/varsByReferenceAndValue.py
@@ -1,3 +1,19 @@
+#RULE TO LIVE BY:
+#Immutable data types = int, float, strings, booleans, and sets
+    #if a var is immutable, then it doesn't matter if we use
+    #an = to copy it to another variable. The value cannot change
+    #so, we can change it anywhere and teh other varaibles will
+    #not be affected
+#Mutable data types = lists, dictionaries, sets, objects
+    #if a var is mutable, then you should expect that if you make a
+    #copy with an = sign, then anywhere you change it, will change
+    #it everywhere
+    #x = []
+    #y = x
+    #x.append(1) 
+    #you have changed y
+
+
 
 # #id will give us a unique integer to represent that variable
 # x = 3
@@ -14,17 +30,30 @@
 # print(id(y))
 # print(id(z)) #Woah. Even though x and z are never associated, Python is smart. It uses the same memory to reference an unchangable integer
 
-x = []
-print(id(x)) #x hasn't changed, its value has
-x.append(1)
-print(id(x))
-y = x 
-print(id(y))
-x.append(2) #x is adding a number
-y.append("Hello") #y is adding a number
-#lists are mutable!!! Python will not change the id (place in memory)
-#it will change the thing in memory and x and y are changing
-#the same thing
-print(id(x))
-print(id(y))
+# x = []
+# print(id(x)) #x hasn't changed, its value has
+# x.append(1)
+# print(id(x))
+# y = x 
+# print(id(y))
+# x.append(2) #x is adding a number
+# y.append("Hello") #y is adding a number
+# #lists are mutable!!! Python will not change the id (place in memory)
+# #it will change the thing in memory and x and y are changing
+# #the same thing
+# print(id(x))
+# print(id(y))
 
+def fight(monster):
+    #based on when we talked about scope (local and global)
+    #will this thing be changable...
+    #remember... when we pass a variable into a function
+    #if it is mutable, we pass the reference
+    monster.append(5)
+    print(id(monster))
+
+enemies = [1,2,3]
+print(id(enemies))
+fight(enemies) #we are passing a global, to a function
+print(id(enemies))
+print(enemies)
