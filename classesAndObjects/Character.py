5d117379fcda6c29c9b26035522407e404f5a480
Robert Bunch <rdbunch@gmail.com>
2023-09-06 11:51:53 -0400

    Ready for 84 - added dunder

--- classesAndObjects/Character.py
+++ classesAndObjects/Character.py
@@ -1,11 +1,19 @@
 
 
 class Character():
-    name = "The Hero"
-    hp = 10
+    #python has dunder method
+    #dunder = a method that starts and ends with __
+    #dunder methods run at special times, i.e. we do not call them
+    def __init__(self):
+        #init is a dunder method that belongs in a class
+        #it is also known in oop as the constructor method
+        #it runs ONE time. When a new object is created
+        self.name = "The Hero"
+        self.hp = 10
 
     #all methods in a class MUST take self as the first param
     def print_name(self):
-        print(f"Hello from Character class, {name}")
+        # global name #global is ALWAYS neccessary if the variable is not defined localally
+        print(f"Hello from Character class, {self.name}")
 
 
--- classesAndObjects/objects101.py
+++ classesAndObjects/objects101.py
@@ -3,6 +3,8 @@ from Character import Character
 #the_hero is an object. What type of object? A Character object
 #Character is the class
 the_hero = Character()
+goblin = Character()
+zombie = Character()
 print(the_hero)
 #unlike dictionaries, we can go straight to our
 # "attributes" which are variables and methods/functions
@@ -11,3 +13,5 @@ print(the_hero)
 #python, whether we like it or not, is ALWAYS
 #going to send "self" to any object method
 the_hero.print_name() #uhh, do we pass it something here, or not?
+goblin.print_name()
+zombie.print_name()
