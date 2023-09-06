6339627d2caaf1522f9e651f2a07727fe7d1b69e
Robert Bunch <rdbunch@gmail.com>
2023-09-06 10:52:23 -0400

    Project Euler folder pe1.py

---
+++ projectEuler/pe1.py
@@ -0,0 +1,17 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+# we get 3, 5, 6,and 9. The sum of these multiples is 23.
+## natural numbers = non-negative integers. 1-9, integers
+
+# Find the sum of all the multiples of 3 or 5 below 1000.
+sum_total = 0 #init sum of our multiples if 3 or 5
+for i in range(1,1000):
+    #is i divisible (evenly) by 3 or 5
+    if(i % 3 == 0 or i % 5 == 0):
+        #it is a match!
+        sum_total += i
+        # print(f"{i} is divisible by 3 or 5")
+print(sum_total)
+
+#ridiculous 1 line answer
+x = sum([n for n in range(1,1000) if n % 3 == 0 or n % 5 == 0])
+print(x)
