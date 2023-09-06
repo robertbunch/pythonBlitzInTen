9e0db6c980cb18e915bd79bd13af1026cb3682da
Robert Bunch <rdbunch@gmail.com>
2023-09-06 12:22:00 -0400

    Ready for 85 - accept param

--- classesAndObjects/Character.py
+++ classesAndObjects/Character.py
@@ -1,14 +1,19 @@
 
+#self is a special key word that is used to reference
+#the object's stuff. It is used in the class, NOT
+#by the object, becuase the object knows it's variable name (the_hero, goblin, zombie)
+#the class just knows it's an object, so thje class
+#must use "self" to reference the object
 
 class Character():
     #python has dunder method
     #dunder = a method that starts and ends with __
     #dunder methods run at special times, i.e. we do not call them
-    def __init__(self):
+    def __init__(self,character_name):
         #init is a dunder method that belongs in a class
         #it is also known in oop as the constructor method
         #it runs ONE time. When a new object is created
-        self.name = "The Hero"
+        self.name = character_name
         self.hp = 10
 
     #all methods in a class MUST take self as the first param
--- classesAndObjects/objects101.py
+++ classesAndObjects/objects101.py
@@ -2,10 +2,11 @@ from Character import Character
 
 #the_hero is an object. What type of object? A Character object
 #Character is the class
-the_hero = Character()
-goblin = Character()
-zombie = Character()
+the_hero = Character("Axe")
+goblin = Character("a simple goblin")
+zombie = Character("a brain seeking zombie")
 print(the_hero)
+print(f"The hero has {the_hero.hp} hp.")
 #unlike dictionaries, we can go straight to our
 # "attributes" which are variables and methods/functions
 #we use . to get to our object "stuff"
@@ -15,3 +16,4 @@ print(the_hero)
 the_hero.print_name() #uhh, do we pass it something here, or not?
 goblin.print_name()
 zombie.print_name()
+print(the_hero.name) #self.name in the class, variable_name.name in the place where the object exists
