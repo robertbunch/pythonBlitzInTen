89f24ee5405070ae5f93eb33c9d5b46a568d1cdf
Robert Bunch <rdbunch@gmail.com>
2023-09-06 12:29:19 -0400

    ready for 85 - pass in another var to init - change object var values

--- classesAndObjects/Character.py
+++ classesAndObjects/Character.py
@@ -9,12 +9,12 @@ class Character():
     #python has dunder method
     #dunder = a method that starts and ends with __
     #dunder methods run at special times, i.e. we do not call them
-    def __init__(self,character_name):
+    def __init__(self,character_name,hp):
         #init is a dunder method that belongs in a class
         #it is also known in oop as the constructor method
         #it runs ONE time. When a new object is created
         self.name = character_name
-        self.hp = 10
+        self.hp = hp
 
     #all methods in a class MUST take self as the first param
     def print_name(self):
--- classesAndObjects/objects101.py
+++ classesAndObjects/objects101.py
@@ -2,9 +2,9 @@ from Character import Character
 
 #the_hero is an object. What type of object? A Character object
 #Character is the class
-the_hero = Character("Axe")
-goblin = Character("a simple goblin")
-zombie = Character("a brain seeking zombie")
+the_hero = Character("Axe",10)
+goblin = Character("a simple goblin",8)
+zombie = Character("a brain seeking zombie",10)
 print(the_hero)
 print(f"The hero has {the_hero.hp} hp.")
 #unlike dictionaries, we can go straight to our
@@ -13,7 +13,18 @@ print(f"The hero has {the_hero.hp} hp.")
 
 #python, whether we like it or not, is ALWAYS
 #going to send "self" to any object method
-the_hero.print_name() #uhh, do we pass it something here, or not?
-goblin.print_name()
-zombie.print_name()
-print(the_hero.name) #self.name in the class, variable_name.name in the place where the object exists
+# the_hero.print_name() #uhh, do we pass it something here, or not?
+# goblin.print_name()
+# zombie.print_name()
+# print(the_hero.name) #self.name in the class, variable_name.name in the place where the object exists
+
+zombie.hp -= 3
+print(f"{zombie.name} has taken 3 damage. Now has {zombie.hp}.")
+print(f"{the_hero.name} has {the_hero.hp} hp.")
+the_hero.hp -= 2
+print(f"{the_hero.name} has taken 2 damage. {the_hero.name} now has {the_hero.hp} hp.")
+goblin.hp -= 3
+print(f"{goblin.name} has taken 3 damage. Now has {goblin.hp}.")
+
+
+
