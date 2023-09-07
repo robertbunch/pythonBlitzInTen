52faf21175ccf7260f07c7d65ee789334de5c9ba
Robert Bunch <rdbunch@gmail.com>
2023-09-07 17:00:54 -0400

    Ready for 93 - added IPhone class

--- classesAndObjects/IPhone.py
+++ classesAndObjects/IPhone.py
@@ -5,8 +5,11 @@ from Phone import Phone
 #we want the methods and attributes that Phones have
 #we pass the parent/super class to the ()
 class IPhone(Phone):
-    def __init__(self):
+    def __init__(self,model,carrier,contacts):
         self.os = "iOS"
         self.make = "Apple"
+        #super is a built in function to Python
+        #super = the parent class 
+        super().__init__(self.make,model,carrier,self.os,contacts)
     def open_facetime(self):
         print("Opening Facetime...")
--- classesAndObjects/inheritance.py
+++ classesAndObjects/inheritance.py
@@ -6,11 +6,16 @@ from IPhone import IPhone
 
 jims_phone = Phone('Apple','iphone 9','Verizon',"iOS",["Jamie"])
 cedrics_phone = Phone('Google','Pixel 7 Pro','EE',"Android",["Sherlock","Marie"])
-ravis_phone = IPhone()
+ravis_phone = IPhone("iphone 13",'Vodaphone',[])
 
 print(jims_phone.make)
 print(jims_phone.contacts)
 cedrics_phone.call_first_contact()
 print(ravis_phone.os)
 print(ravis_phone.make)
+print("Calling contact from Ravis phone...")
+ravis_phone.call_first_contact()
 print(ravis_phone.carrier)
+
+ravis_phone.open_facetime()
+# jims_phone.open_facetime()
