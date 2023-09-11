3061160c3c388037a54da89906f3b32bacee83bf
Robert Bunch <rdbunch@gmail.com>
2023-09-11 12:35:27 -0400

    Ready for 108 - add annoy_shopkeeper

--- class_based_text_rpg/Shop.py
+++ class_based_text_rpg/Shop.py
@@ -41,6 +41,7 @@ class Shop():
     def display(self,hero,game_settings):
         #display the shop options to the player
         keep_shopping = True #bool to keep the shop loop running
+        annoy_tries = 0
         while(keep_shopping):
             print(f"Thou hast entered the shop with {game_settings.colors['yellow']}{hero.gold} gold{game_settings.colors['normal']}.")
             counter = 0
@@ -48,5 +49,23 @@ class Shop():
                 counter += 1 #counter is to give the user the number to push
                 #each item is a {} in self.items
                 print(f"{counter}. {item['item_name']}, {item['cost']} - {item['desc']}")
-            
-            keep_shopping = False
+            shop_action = input("The keeper of the shop is gruff and short with you. Well? What do you want? > ")
+            #to get the index in self.items that matches what the user entered, we -1 because the indicies start at 0
+            index_in_items = int(shop_action) - 1
+            #get the item from self.items based on the index
+            item_to_buy = self.items[index_in_items]
+            #does the user have the money to buy the item?
+            if(hero.gold >= item_to_buy["cost"]):
+                #the user has enough gold!
+                print("")
+            else:
+                #the user does NOT have enough gold
+                annoy_tries += 1 #each time the player tries to buy something they do not have the money for, increase annoy_tries
+                if(annoy_tries == 1):
+                    #just one time making a mistake
+                    print(f"The keeper responds, 'dont waste my time. Go earn some coin, and come back'")
+                elif(annoy_tries == 2):
+                    print(f"{game_settings.colors['blue']}The shop owner eyes you angrily... You cannot afford that item.{game_settings.colors['normal']}")
+                elif(annoy_tries == 3):
+                    print(f"{game_settings.colors['red']}The shopkeeper howls. He grabs you by the coller and throws you out of the shop!{game_settings.colors['normal']}")
+                    keep_shopping = False
