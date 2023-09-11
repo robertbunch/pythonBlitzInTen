62eae29e40b1f110a27146945e971cff9acd9d48
Robert Bunch <rdbunch@gmail.com>
2023-09-11 10:49:36 -0400

    Add shop & items dictionary

---
+++ class_based_text_rpg/Shop.py
@@ -0,0 +1,42 @@
+
+
+class Shop():
+    def __init__(self):
+        self.items = [
+            {
+                "item_name" : "Health Potion",
+                "py_name" : "health_potion",
+                "desc" : "Refresh all hit points",
+                "cost" : 10,
+            },
+            {
+                "item_name" : "Magic Sword",
+                "py_name" : "magic_sword",
+                "desc" : "Attack Power +15%",
+                "cost" : 50,
+            },
+            {
+                "item_name" : "Magic Armor",
+                "py_name" : "magic_armor",
+                "cost" : 50,
+                "desc" : "Defense +15%",
+            },
+            {
+                "item_name" : "Evasion Potion",
+                "py_name" : "evasion_potion",
+                "cost" : 20,
+                "desc" : "+20% chance to evade attack",
+            },
+            {
+                "item_name" : "Magic Carpet",
+                "py_name" : "magic_carpet",
+                "cost" : 30,
+                "desc" : "Guarantee fight escape",
+            },
+            {
+                "item_name" : "Leave",
+                "py_name" : "leave",
+                "cost" : "",
+                "desc" : "",
+            },
+        ]
