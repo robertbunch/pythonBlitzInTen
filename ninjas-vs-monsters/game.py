9b830c9fb8625c600948a5f9e244f05edabd2bcf
Robert Bunch <rdbunch@gmail.com>
2023-09-18 16:29:18 -0400

    Ready for 122 - add init and display info

--- ninjas-vs-monsters/game.py
+++ ninjas-vs-monsters/game.py
@@ -1,2 +1,15 @@
 import pygame
-print("Pygame successfully imported!")
+# print("Pygame successfully imported!")
+pygame.init() #initialize all the pygame stuff
+#get the display info... Info is a class!
+display_info = pygame.display.Info()
+screen_size = (display_info.current_w, display_info.current_h)
+screen = pygame.display.set_mode(screen_size)
+game_on = True
+while(game_on): #game_loop
+    for event in pygame.event.get(): #pygame has events for us to listen to
+        if(event.type == pygame.QUIT):
+            print(event)
+            game_on = False
+    screen.fill("purple")
+    pygame.display.flip()
