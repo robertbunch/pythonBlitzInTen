7d2cf10ab10ae38a19097770d9a1da83efffa277
Robert Bunch <rdbunch@gmail.com>
2023-09-19 11:14:24 -0400

    Ready for 125 - Background class - scale

---
+++ ninjas-vs-monsters/Background.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Background():
+    def __init__(self,screen,screen_size):
+        #grab our image off the hard drive and load it into pygame
+        self.background_image = pygame.image.load('./images/background4.png')
+        #change the size of the image
+        #scale method! scale takes 2 args:
+        #1. what to scale
+        # 2.tuple of the new coords
+        self.background_image = pygame.transform.scale(self.background_image,screen_size)
+        self.background_rect = self.background_image.get_rect()
+        # print(background_rect)
+
+        #blit = block image transfer
+        #blit takes 2 args: 
+        #1. what to draw
+        #2. where to draw it (x,y coords which we get from get_rect)
+        screen.blit(self.background_image,self.background_rect)
