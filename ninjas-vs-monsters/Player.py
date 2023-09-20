63309cfb4aa616477e7ebd42be88bc8ef2b915a9
Robert Bunch <rdbunch@gmail.com>
2023-09-20 13:04:59 -0400

    Ready for 131 - Load Run and Idle images

--- ninjas-vs-monsters/Player.py
+++ ninjas-vs-monsters/Player.py
@@ -5,6 +5,19 @@ class Player():
         self.screen = screen
         self.x = 200
         self.y = 200
+        # anim_image is the number cooreponding to the number in the filename of the that image
+        self.anim_image = 0
+
+        self.image_types = {
+            "Idle" : [],
+            "Run" : [],
+        }
+
+        for image_type in self.image_types:
+            for i in range(0,10):
+                image_to_load = pygame.image.load(f"./images/ninja/{image_type}__00{i}.png")
+                image_to_load = pygame.transform.scale_by(image_to_load,.35)
+                self.image_types[image_type].append(image_to_load)
         #load the image from the hd
         self.image = pygame.image.load("./images/ninja/Idle__000.png")
         self.image = pygame.transform.scale_by(self.image,.35)
