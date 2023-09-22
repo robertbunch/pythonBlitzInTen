ed262bf966ca3c9cf0ae02657d907b5f3a36ece8
Robert Bunch <rdbunch@gmail.com>
2023-09-22 16:43:29 -0400

    Ready for 146 - Update - ork and troll starter stats

---
+++ ninjas-vs-monsters/ork_and_troll_starter_stats.py
@@ -0,0 +1,43 @@
+        troll_stats = [{
+            "hp" : 10,
+            "speed" : 3,
+            "attack" : 2,
+            "name" : "troll1",
+            "images" : Troll.troll_images['troll1'],
+        },
+        {
+            "hp" : 15,
+            "speed" : 2.4,
+            "attack" : 3,
+            "name" : "troll2",
+            "images" : Troll.troll_images['troll2']
+        },
+        {
+            "hp" : 20,
+            "speed" : 2,
+            "attack" : 4,
+            "name" : "troll3",
+            "images" : Troll.troll_images['troll3']
+        }]
+
+    ork_stats = [{
+            "hp" : 20,
+            "speed" : 1.2,
+            "attack" : 3,
+            "name" : "ork1",
+            "images" : Ork.ork_images['ork1'],
+        },
+        {
+            "hp" : 25,
+            "speed" : 1,
+            "attack" : 3,
+            "name" : "ork2",
+            "images" : Ork.ork_images['ork2'],
+        },
+        {
+            "hp" : 15,
+            "speed" : 1,
+            "attack" : 4,
+            "name" : "ork3",
+            "images" : Ork.ork_images['ork3'],
+        }]
