1f3b749a834b25fe77591ca657fb7286ce882c3d
Robert Bunch <rdbunch@gmail.com>
2023-12-05 17:36:30 -0500

    pe15 rewrite

--- projectEuler/pe15.py
+++ projectEuler/pe15.py
@@ -1,15 +1,35 @@
 import time
-x=int(input("Enter X of grid: "))
-y=int(input("Enter Y of grid: "))
-start = time.time()
-route_matrix = list(range(1,x+2))
-print(route_matrix)
-while route_matrix [1]!=y+1:
-    i=0
-    print(route_matrix)
-    for n in route_matrix [1:]:
-        i+=1
-        route_matrix [i]=n+route_matrix [i-1]
-print(f"There are {route_matrix [-1]} routes in {x}x{y} grid!")
-end = time.time() - start
-print("Runtime =", end)
+start_time = time.time() #get our starting time
+# Answer: 137846528820
+def findRoutes(gridSize):
+    routeMatrix = []
+    routeMatrix = [[1] * gridSize] * gridSize
+    # 20x20 grid of 1s
+
+    for i in range(0,gridSize):
+        for j in range(0,gridSize):
+            if(i == 0):
+                routeMatrix[i][j] = routeMatrix[i][j-1]
+            elif(j == 0):
+                routeMatrix[i][j] = routeMatrix[i-1][j]        
+            else:
+                routeMatrix[i][j] = routeMatrix[i][j-1] + routeMatrix[i-1][j]
+            print(i,j,routeMatrix[i][j])
+        print(routeMatrix)    
+        # routeMatrix[i] = 2 * routeMatrix[i-1]
+#     }
+    print(routeMatrix)
+    return routeMatrix
+# }
+
+
+# console.time('grid')
+gridSize = 3
+n = findRoutes(gridSize);
+print(n)
+print(n[1][1])
+# console.timeEnd('grid')
+
+# end_time = time.time() #get our ending time
+# print(f"--- Number of seconds to solve {time.time() - start_time}")
+ 
