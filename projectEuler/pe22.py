64bb86ac0211855bce9badd91851360faa6a3c2c
Robert Bunch <rdbunch@gmail.com>
2024-02-13 10:35:38 -0500

    pe 22 needed to be re-committed

--- projectEuler/pe22.py
+++ projectEuler/pe22.py
@@ -1,33 +1,35 @@
-from radixPE22 import radix_sort
-
 import time
+from radixPE22 import radix_sort
 start_time = time.time() #get our starting time
 # Answer: 871198282
 
-f = open("names.txt", "r")
-names = f.read().split(',')
-f.close()
-
+# opening our file for reading
+names_file = open('./names.txt')
+# read file into a string and split into a list
+names = names_file.read().split(',')
+names_file.close() #good practice
+# parse our list so the " are gone!
 names = [name.strip('"') for name in names]
 
-# sorted_names = sorted(names)
+# for name in names:
+#     print(name)
+
 sorted_names = radix_sort(names)
+# sorted_names = sorted(names) #sorted is the default python sort
 
-sum = 0
-for i,name in enumerate(sorted_names):
-    this_name = 0
+sum = 0 #our answer!
+# loop through each name and find the value of the char
+for i, name in enumerate(sorted_names):
+    this_name_val = 0 #init this name char value
+    # loop through the char in this name
     for char in name:
-        this_name += ord(char) - ord("A") + 1
-    sum += this_name * (i+1)
-
+        # A = 1, B = 2, C = 3, ... we use A as our 
+        # base, and offset by it, +1
+        this_name_val += ord(char) - ord('A') + 1
+    sum += this_name_val * (i + 1)
 print(sum)
+
 
 end_time = time.time() #get our ending time
 print(f"--- Number of seconds to solve {time.time() - start_time}")
-
-# import time
-# start_time = time.time() #get our starting time
-# sorted_names = radix_sort(names)
-# print(sorted_names[0])
-# print(f"--- Number of seconds to solve {time.time() - start_time}")
 
