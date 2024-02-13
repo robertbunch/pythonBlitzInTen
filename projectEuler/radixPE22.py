da0eb8e764df48c28b8cbd7285a8fe7387bff52a
Robert Bunch <rdbunch@gmail.com>
2024-02-13 10:35:55 -0500

    radixpe22 needed to be pushed

--- projectEuler/radixPE22.py
+++ projectEuler/radixPE22.py
@@ -1,30 +1,30 @@
 def bucket_sorter(list,char_position):
     # make our 26 + 1 buckets
     buckets = [[] for i in range(27)]
-    # print(buckets)
     # go through each word and grab the letter at char_position
     # and put it in the right bucket
+    result = [] #this is going to contain all the words that are still on a ' '
     for word in list:
         if(word[char_position] == ' '):
-            # this is a blank. It goes in bucket 26
-            buckets[26].append(word)
+            # this is a blank. It goes in the first bucket (list)
+            result.append(word)
         else:
             bucket_index = ord(word[char_position]) - ord('A')
             # print(bucket_index)
             buckets[bucket_index].append(word)
     # print(buckets)
     # step 2 is concat our buckets into 1 list
-    result = []
-    for bucket in buckets:
-        result += bucket
-    return result
 
+    for bucket in buckets:
+        result += bucket #concat all the buckets onto the first
+    return result
 def radix_sort(list_to_sort):
     # get the longest element
     longest = max(len(str) for str in list_to_sort)
-    print(longest)
+    # print(longest)
     # pad all the strings shorter than "longest"
     list_to_sort = [str.ljust(longest,' ') for str in list_to_sort] 
+    # print(list_to_sort)
     # loop through the characters one at a time, and bucket
     for i in range(longest - 1, -1, -1):
         list_to_sort = bucket_sorter(list_to_sort,i)
